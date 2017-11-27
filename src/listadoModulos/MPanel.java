8ef47ccd001c88f79c653c7140746b97a1ef7493
RhoLouh <rholouh@gmail.com>
2017-11-27 19:58:31 +0100

    Primera subidas

--- src/listadoModulos/MPanel.java
+++ src/listadoModulos/MPanel.java
@@ -2,9 +2,22 @@ package listadoModulos;
 
 import java.awt.Panel;
 
+import javax.swing.BoxLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 
 public class MPanel extends Panel {
+	private JTextArea listaM = new JTextArea(20,20);
+	private JScrollPane OrigScroll = new JScrollPane(listaM, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+			JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	public MPanel(){
+		this.setLayout(new BoxLayout(this, 2));
+		this.add(listaM);
+		
+		//JPanel Buttons = new 
+	}
 	public void setController(MController ctr) {
-
+		
 	}
 }
