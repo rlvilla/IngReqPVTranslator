d63b25919db1f000875ec6d7b23c3ee871eb9cc7
pelocho <oscarmorente98@gmail.com>
2017-11-28 12:05:08 +0100

    Grafico medidas

--- src/listadoMedidas/MedidasPanel.java
+++ src/listadoMedidas/MedidasPanel.java
@@ -1,20 +1,31 @@
 package listadoMedidas;
 
-import java.awt.Panel;
-import javax.swing.BoxLayout;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import java.awt.*;
+import javax.swing.*;
 
 public class MedidasPanel extends Panel{
     private JTextArea ListaMedidas = new JTextArea(20, 20);
-    private JScrollPane OrigniScroll = new JScrollPane(ListaMedidas, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    public MedidasPanel(){
-        this.setLayout(new BoxLayout(this,2));
-        this.add(ListaMedidas);
+    private JScrollPane ListaMedScroll = new JScrollPane(ListaMedidas, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    private JButton ListMod = new JButton("Listado Modulos");
+    private JButton AbrirCurv = new JButton("Abrir Curva");
 
-        //JPanel Buttons = new
+    public MedidasPanel(){
+        this.setLayout(new BoxLayout(this,1));
+        JPanel Lista = new JPanel();
+        ListaMedidas.setEditable(false);
+        Lista.setLayout(new BoxLayout(Lista,1));
+        Lista.add(ListaMedScroll, BorderLayout.NORTH);
+        Lista.add(ListaMedidas, BorderLayout.SOUTH);
+
+        JPanel Botones = new JPanel();
+        Botones.setLayout(new GridLayout(1,2));
+        Botones.add(ListMod);
+        Botones.add(AbrirCurv);
+
+        this.add(Lista);
+        this.add(Botones);
     }
     public void setController(MedidasController ctr){
-
+        String s = ListaMedidas.getSelectedText();
     }
 }
