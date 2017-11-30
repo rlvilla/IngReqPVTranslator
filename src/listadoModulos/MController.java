be61ecaf6dda4feacaa3a1059b2cfd620e405139
RhoLouh <rholouh@gmail.com>
2017-11-30 11:26:23 +0100

    file chooser

--- src/listadoModulos/MController.java
+++ src/listadoModulos/MController.java
@@ -1,7 +1,9 @@
 package listadoModulos;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MController implements ActionListener {
 	private MPanel panel;
@@ -13,10 +15,21 @@ public class MController implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals(MPanel.CARGARMODULO)) {
-			//TODO
-		}
+            JFileChooser jfile = new JFileChooser();
+            int ret = jfile.showOpenDialog(panel);
+
+            if (ret == JFileChooser.APPROVE_OPTION){
+                File file = jfile.getSelectedFile();
+            }
+        }
+
 		if(e.getActionCommand().equals(MPanel.CARGARMEDIDAS)){
-		    //TODO
+            JFileChooser jfile = new JFileChooser();
+            int ret = jfile.showOpenDialog(panel);
+
+            if (ret == JFileChooser.APPROVE_OPTION){
+                File file = jfile.getSelectedFile();
+            }
         }
         if(e.getActionCommand().equals(MPanel.ELIMINARMODULO)){
             //TODO
