1245faaa09b023c40d5a49e226df3dc786668f13
Samuel Jurado Quintana <samueljurado12@gmail.com>
2018-01-07 02:39:00 +0100

    a

--- src/Modelo/Modulo.java
+++ src/Modelo/Modulo.java
@@ -5,23 +5,19 @@ import java.util.List;
 
 public class Modulo {
     private String name;
-    private float alpha, beta, gamma, kappa;
+    private float alpha, beta, gamma, kappa, rs;
 
     private List<Medida> medidas;
 
-    public Modulo(String name, float alpha, float beta, float gamma, float kappa){
+    public Modulo(String name, float alpha, float beta, float gamma, float kappa, float rs){
         this.name = name;
         this.alpha = alpha;
         this.beta = beta;
         this.gamma = gamma;
         this.kappa = kappa;
+        this.rs = rs;
         medidas = new ArrayList<>();
     }
-
-    public Modulo(String name){
-       this(name,0,0,0,0);
-    }
-
 
     public String toString(){
         return("Name: " +name+ "\nAlpha: "+alpha+"\nBeta: "+beta+"\nGamma: "+gamma+"\nKappa: "+kappa+"\n");
